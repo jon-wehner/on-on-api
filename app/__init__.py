65e7f8bebc130f20226c2f2ae4ef6c68b20fd4b5
Jon Wehner <jonjwehner@gmail.com>
2022-06-22 11:00:55 -0700

    flask config

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 import os
 
-from flask import Flask 
+from flask import Flask
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
