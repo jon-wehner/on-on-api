620a58f73ba940d35e082ae5c9bbc3a40cb9fd10
Jon Wehner <jonjwehner@gmail.com>
2022-06-29 22:54:00 -0700

    migration setups

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
+
+
+from .models import db
+from .config import Config
 
 
 def create_app(test_config=None):
@@ -9,11 +14,16 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
     )
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_object(Config)
     else:
         app.config.from_mapping(test_config)
+
+    db.init_app(app)
+    Migrate(app, db)
 
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    return app
