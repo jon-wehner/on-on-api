90505fb7d20c7064e2e770a69349efaba5d110ca
Jon Wehner <jonjwehner@gmail.com>
2022-06-30 23:29:02 -0700

    flask login and cors setup

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ import os
 
 from flask import Flask
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_cors import CORS
 
 
-from .models import db
+from .models import db, User
 from .seeds import seed, seed_commands
 from .config import Config
+
+login_manager = LoginManager()
 
 
 def create_app(test_config=None):
@@ -19,10 +23,19 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
+    CORS(app)
+
     app.cli.add_command(seed_commands)
 
     db.init_app(app)
     Migrate(app, db)
+
+    # Flask Login
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     try:
         os.makedirs(app.instance_path)
